bb2bce10fcc80a1370d307e522e5f334cb8dad49
joshuapchek-a11y <joshuapchek@gmail.com>
2026-04-22 14:20:15 +1000

    testing a redone index

--- script.js
+++ script.js
@@ -7,4 +7,22 @@
     })
   })
 
-  
+$(document).ready(function() {
+  const images = $('.slideshow img');
+  if (images.length > 0) {
+    let current = 0;
+    function showImage(index) {
+      images.css('opacity', 0);
+      const img = images.eq(index);
+      img.css('opacity', 1);
+      // Restart GIF by updating src with timestamp
+      const src = img.attr('src').split('?')[0];
+      img.attr('src', src + '?t=' + Date.now());
+    }
+    showImage(0);
+    setInterval(() => {
+      current = (current + 1) % images.length;
+      showImage(current);
+    }, 3000);
+  }
+});
